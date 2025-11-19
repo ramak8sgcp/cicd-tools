113007c2c3c554f4406b0287bdbe097ecb328e79
ramak8sgcp <ramak8sgcp@gmail.com>
2025-11-19 15:40:19 +0530

    update

--- main.tf
+++ main.tf
@@ -4,22 +4,21 @@ module "jenkins" {
   name = "jenkins"
 
   instance_type          = "t3.small"
-  vpc_security_group_ids = ["sg-0fea5e49e962e81c9"]   #replace your SG
-  subnet_id              = "subnet-0ea509ad4cba242d7" #replace your Subnet
+  vpc_security_group_ids = ["sg-0c86d154202a71583"]   # replace your SG
+  subnet_id              = "subnet-08f729b0eec39634f" # replace your Subnet
   ami                    = data.aws_ami.ami_info.id
   user_data              = file("jenkins.sh")
+
   tags = {
     Name = "jenkins"
   }
 
-  # Define the root volume size and type
-  root_block_device = [
-    {
-      volume_size           = 50    # Size of the root volume in GB
-      volume_type           = "gp3" # General Purpose SSD (you can change it if needed)
-      delete_on_termination = true  # Automatically delete the volume when the instance is terminated
-    }
-  ]
+  # Correct root block device (object)
+  root_block_device = {
+    volume_size           = 50
+    volume_type           = "gp3"
+    delete_on_termination = true
+  }
 }
 
 module "jenkins_agent" {
@@ -28,23 +27,22 @@ module "jenkins_agent" {
   name = "jenkins-agent"
 
   instance_type          = "t3.small"
-  vpc_security_group_ids = ["sg-0fea5e49e962e81c9"]
-  subnet_id              = "subnet-0ea509ad4cba242d7"
+  vpc_security_group_ids = ["sg-0c86d154202a71583"]
+  subnet_id              = "subnet-08f729b0eec39634f"
   ami                    = data.aws_ami.ami_info.id
   user_data              = file("jenkins-agent.sh")
+
   tags = {
     Name = "jenkins-agent"
   }
 
-  root_block_device = [
-    {
-      volume_size           = 50    # Size of the root volume in GB
-      volume_type           = "gp3" # General Purpose SSD (you can change it if needed)
-      delete_on_termination = true  # Automatically delete the volume when the instance is terminated
-    }
-  ]
+  # Correct root block device (object)
+  root_block_device = {
+    volume_size           = 50
+    volume_type           = "gp3"
+    delete_on_termination = true
+  }
 }
-
 
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
@@ -54,23 +52,99 @@ module "records" {
 
   records = [
     {
-      name = "jenkins"
-      type = "A"
-      ttl  = 1
-      records = [
-        module.jenkins.public_ip
-      ]
+      name            = "jenkins"
+      type            = "A"
+      ttl             = 1
+      records         = [module.jenkins.public_ip]
       allow_overwrite = true
     },
     {
-      name = "jenkins-agent"
-      type = "A"
-      ttl  = 1
-      records = [
-        module.jenkins_agent.private_ip
-      ]
+      name            = "jenkins-agent"
+      type            = "A"
+      ttl             = 1
+      records         = [module.jenkins_agent.private_ip]
       allow_overwrite = true
     }
   ]
-
 }
+
+
+
+
+
+# module "jenkins" {
+#   source = "terraform-aws-modules/ec2-instance/aws"
+
+#   name = "jenkins"
+
+#   instance_type          = "t3.small"
+#   vpc_security_group_ids = ["sg-0c86d154202a71583"]   #replace your SG
+#   subnet_id              = "subnet-08f729b0eec39634f" #replace your Subnet
+#   ami                    = data.aws_ami.ami_info.id
+#   user_data              = file("jenkins.sh")
+#   tags = {
+#     Name = "jenkins"
+#   }
+
+#   # Define the root volume size and type
+#   root_block_device = [
+#     {
+#       volume_size           = 50    # Size of the root volume in GB
+#       volume_type           = "gp3" # General Purpose SSD (you can change it if needed)
+#       delete_on_termination = true  # Automatically delete the volume when the instance is terminated
+#     }
+#   ]
+# }
+
+# module "jenkins_agent" {
+#   source = "terraform-aws-modules/ec2-instance/aws"
+
+#   name = "jenkins-agent"
+
+#   instance_type          = "t3.small"
+#   vpc_security_group_ids = ["sg-0c86d154202a71583"]
+#   subnet_id              = "subnet-08f729b0eec39634f"
+#   ami                    = data.aws_ami.ami_info.id
+#   user_data              = file("jenkins-agent.sh")
+#   tags = {
+#     Name = "jenkins-agent"
+#   }
+
+#   root_block_device = [
+#     {
+#       volume_size           = 50    # Size of the root volume in GB
+#       volume_type           = "gp3" # General Purpose SSD (you can change it if needed)
+#       delete_on_termination = true  # Automatically delete the volume when the instance is terminated
+#     }
+#   ]
+# }
+
+
+# module "records" {
+#   source  = "terraform-aws-modules/route53/aws//modules/records"
+#   version = "~> 2.0"
+
+#   zone_name = var.zone_name
+
+#   records = [
+#     {
+#       name = "jenkins"
+#       type = "A"
+#       ttl  = 1
+#       records = [
+#         module.jenkins.public_ip
+#       ]
+#       allow_overwrite = true
+#     },
+#     {
+#       name = "jenkins-agent"
+#       type = "A"
+#       ttl  = 1
+#       records = [
+#         module.jenkins_agent.private_ip
+#       ]
+#       allow_overwrite = true
+#     }
+#   ]
+
+# }
